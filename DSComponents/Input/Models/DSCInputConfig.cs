450a3a66f7246299b46dbac9e193e4d87a8851dc
RThompson153 <robbyt15@gmail.com>
2022-09-23 03:07:37 -0500

    Improves inline label.  Adds select icon.  Adds Min/Max checking for select.

--- DSComponents/Input/Models/DSCInputConfig.cs
+++ DSComponents/Input/Models/DSCInputConfig.cs
@@ -10,18 +10,18 @@ namespace DSComponents.Input.Models
 	{
 		public Guid Id {get;set;}
 		public DSCLabelConfig Label { get; set; }
-		//public InputTypes Type { get; set; }
-		public string Name {get;set;}
+		public string Name { get; set; }
 		public List<object> Value { get; set; } = new();
-		public decimal? MinLength { get; set; }
-		public decimal? MaxLength { get; set; }
+		public decimal? Minimum { get; set; }
+		public decimal? Maximum { get; set; }
 		public double? Timeout { get; set; }
 		public bool Multiple { get; set; }
 		public bool Searchable { get; set; }
 		public string TextColor { get; set; } = "text-dark";
 		public string BackgroundColor { get; set; } = "bg-white";
 		public bool Error { get; set; }
-		public bool Expanded {get;set;}
+		public bool Expanded { get; set; }
+		public bool Disabled { get; set; }
 		public bool HasFocus { get; set; }
 		public List<object> SelectItems { get; set; }
 	}
