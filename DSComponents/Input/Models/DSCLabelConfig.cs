9bb7ff83c767a3998166bfd6da8d2f9684317b54
Robert Thompson <Robert.Thompson@clickbond.com>
2022-10-04 15:25:43 -0500

    working

--- DSComponents/Input/Models/DSCLabelConfig.cs
+++ DSComponents/Input/Models/DSCLabelConfig.cs
@@ -5,6 +5,6 @@ namespace DSComponents.Input.Models
 	public class DSCLabelConfig
 	{
 		public string Label { get; set; }
-		public LabelPosition Position { get; set; }
+		public Position Position { get; set; }
 	}
 }
