2c281ec614f91e593220b3d53781b8afb1253bf2
RThompson153 <robbyt15@gmail.com>
2022-12-18 15:13:00 -0600

    Adds basic radio button functionality.

--- DSComponents/Models/Internal/RadioGroup.cs
+++ DSComponents/Models/Internal/RadioGroup.cs
@@ -1,8 +1,16 @@
-﻿namespace DSComponents
+﻿using System.Collections.Generic;
+
+namespace DSComponents
 {
 	public class RadioGroup
 	{
+		private Dictionary<string, bool> _options;
 		internal string GroupName { get; set; }
 		internal string SelectedValue { get; set; }
+		internal Dictionary<string, bool> Options 
+		{ 
+			get => _options ?? new Dictionary<string, bool>();
+			set => _options = value;
+		}
 	}
 }
